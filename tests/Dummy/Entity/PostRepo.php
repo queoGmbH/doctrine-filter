00e8c12bc7ed035d999d8ac5e4d476287a5ad144
Thomas Uhlmann <tho.uhlmann@gmail.com>
2016-01-30 21:23:32 +0100

    Remove trait

--- tests/Dummy/Entity/PostRepo.php
+++ tests/Dummy/Entity/PostRepo.php
@@ -5,7 +5,6 @@ namespace Fludio\DoctrineFilter\Tests\Dummy\Entity;
 use Doctrine\ORM\EntityRepository;
 use Fludio\DoctrineFilter\Filter\FilterBuilder;
 use Fludio\DoctrineFilter\Filter\FilterInterface;
-use Fludio\DoctrineFilter\Filter\OnSteroids;
 
 class PostRepo extends EntityRepository
 {
