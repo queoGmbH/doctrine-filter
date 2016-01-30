45fa8643a75211e9988ed0e3f32c1dc0a6e41f5c
Thomas Uhlmann <tho.uhlmann@gmail.com>
2016-01-30 20:10:23 +0100

    Fix FilterBuilderTest

--- tests/Filter/FilterBuilderTest.php
+++ tests/Filter/FilterBuilderTest.php
@@ -2,21 +2,33 @@
 
 namespace Fludio\DoctrineFilter\Tests;
 
+use Fludio\DoctrineFilter\Filter\FilterBuilder;
+use Fludio\DoctrineFilter\Filter\Type\EqualFilterType;
 use Fludio\DoctrineFilter\Tests\Dummy\Entity\Post;
 use Fludio\DoctrineFilter\Tests\Dummy\Filter\PostFilter;
 use Fludio\DoctrineFilter\Tests\Dummy\LoadFixtures;
 use Fludio\DoctrineFilter\Tests\Dummy\TestCase;
+use Fludio\DoctrineFilter\Tests\Dummy\Traits\TestFilterTrait;
 
 class FilterBuilderTest extends TestCase
 {
-    use LoadFixtures;
+    use LoadFixtures, TestFilterTrait;
+
+    public function getFilterDefinition()
+    {
+        return function (FilterBuilder $builder) {
+            $builder
+                ->add('title', EqualFilterType::class)
+                ->add('content', EqualFilterType::class);
+        };
+    }
 
     /** @test */
     public function it_tracks_parameters_by_their_own_value()
     {
-        $posts = $this->em->getRepository(Post::class)->filter(new PostFilter(), [
+        $posts = $this->em->getRepository(Post::class)->filter($this->filter, [
             'title' => 'Post title',
-            'content_same' => 'My post content!'
+            'content' => 'My post content!'
         ]);
 
         $this->assertCount(1, $posts);
